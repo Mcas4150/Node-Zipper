205a4f4f2ee4db7ef6c267c72aa21db0f7d44690
Mcas4150 <mikecassidy0000@gmail.com>
2021-04-22 13:13:12 -0400

    add compression progress

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const output = fs.createWriteStream(zipPath);
 
 const multiBar = new cliProgress.MultiBar(
   {
-    format: "{bar} {percentage}% | ETA: {eta}s | {value}/{total}",
+    format: `{bar} | | {percentage}% | ETA: {eta}s | {value}/{total}`,
     barCompleteChar: "\u2588",
     barIncompleteChar: "\u2591",
     clearOnComplete: false,
@@ -25,11 +25,16 @@ const multiBar = new cliProgress.MultiBar(
   cliProgress.Presets.shades_classic
 );
 
-const downloadFile = (url, stream) => {
+let zipProgressBar = multiBar.create(2, 0);
+zipProgressBar.update(0);
+
+const downloadFile = (url, stream, fileName) => {
   https.get(url, (res) => {
     let receivedBytes = 0;
     let totalBytes = res.headers["content-length"];
+
     let progressBar = multiBar.create(totalBytes, 0);
+    multiBar.file = fileName;
     res
       .on("data", (chunk) => {
         receivedBytes += chunk.length;
@@ -39,8 +44,8 @@ const downloadFile = (url, stream) => {
   });
 };
 
-downloadFile(url, stream1);
-downloadFile(url2, stream2);
+downloadFile(url, stream1, "file1");
+downloadFile(url2, stream2, "file2");
 
 // Archive Compression
 const archive = archiver("zip", {
@@ -51,8 +56,19 @@ archive.on("error", function (err) {
   throw err;
 });
 
+archive.on("end", function () {
+  console.log("Zip file is generated");
+});
+
+output.on("close", function () {
+  console.log(archive.pointer() + " total bytes");
+});
+
+archive.on("progress", function (event) {
+  zipProgressBar.update(event["entries"]["processed"]);
+});
+
 archive.append(stream1, { name: "img1.jpg" });
 archive.append(stream2, { name: "img2.jpg" });
 archive.finalize();
 archive.pipe(output);
-
